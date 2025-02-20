9f864b66187e8f029a3982556d79bf936b7fa1c9
Elijah Karori Orito <elijakarori23@gmail.com>
2025-02-20 21:57:43 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="robots" content="follow, index" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="google-adsense-account" content="ca-pub-6852634338900661">
+        <meta name="google-adsense-account" content="ca-pub-6852634338900661"/>
       </Head>
       <body className="bg-white text-gray-700 antialiased">
         <Main />
