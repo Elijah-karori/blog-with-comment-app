d11b1efc068e22f4053e7d7d0b7bc792feb7bd84
Elijah Karori Orito <elijakarori23@gmail.com>
2025-02-20 21:38:24 +0300

    Update index.tsx

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -9,6 +9,9 @@ export default function NotePage({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Container>
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6852634338900661"
+     crossorigin="anonymous"></script></head>
       {allPosts.length ? (
         allPosts.map((post) => (
           <article key={post.slug} className="mb-10">
