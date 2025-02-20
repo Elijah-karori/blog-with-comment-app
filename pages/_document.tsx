7c568f521d7b78a147ac86871097eb7871ab8629
Elijah Karori Orito <elijakarori23@gmail.com>
2025-02-20 21:23:24 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="robots" content="follow, index" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6852634338900661"
+     crossorigin="anonymous"></script>
       </Head>
       <body className="bg-white text-gray-700 antialiased">
         <Main />
