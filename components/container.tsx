5d93b0e2c7421759cbe0cfaf94cd3bbd894eddf6
Elijah Karori Orito <elijakarori23@gmail.com>
2025-02-24 23:22:16 +0300

    Update container.tsx

--- components/container.tsx
+++ components/container.tsx
@@ -1,7 +1,14 @@
+import Script from 'next/script'
+import Head from 'next/head'
 type ContainerProps = {
   children: React.ReactNode;
 };
 
 export default function Container({ children }: ContainerProps) {
-  return <div className="container max-w-2xl m-auto px-4">{children}</div>;
+  return <div className="container max-w-2xl m-auto px-4">
+    <head>
+    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6852634338900661"
+     />
+    </head>
+    {children}</div>;
 }
